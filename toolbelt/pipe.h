bc28a810d3ff91af2b921be8698dfcede64fcc57
David Allison <david.s.allison@gmail.com>
2025-08-24 10:48:42 -0700

    Reads and writes and tests

--- toolbelt/pipe.h
+++ toolbelt/pipe.h
@@ -3,6 +3,7 @@
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_format.h"
+#include "coroutine.h"
 #include "toolbelt/fd.h"
 
 #include <errno.h>
@@ -43,9 +44,35 @@ public:
     write_.ForceClose();
   }
 
-private:
+  absl::Status SetNonBlocking(bool read, bool write) {
+    if (read) {
+      if (absl::Status s = read_.SetNonBlocking(); !s.ok()) {
+        return s;
+      }
+    }
+    if (write) {
+      if (absl::Status s = write_.SetNonBlocking(); !s.ok()) {
+        return s;
+      }
+    }
+    return absl::OkStatus();
+  }
+
+  absl::StatusOr<size_t> GetPipeSize();
+  absl::Status SetPipeSize(size_t size);
+  
+  absl::StatusOr<ssize_t> Read(char *buffer, size_t length,
+                               co::Coroutine *c = nullptr);
+  absl::StatusOr<ssize_t> Write(const char *buffer, size_t length,
+                                co::Coroutine *c = nullptr);
+
+protected:
   FileDescriptor read_;
   FileDescriptor write_;
+  // We really don't want coroutines to interleave reads or writes on the
+  // same pipe.  If they do that, the data will be interleaved and confused.
+  bool read_in_progress_ = false;
+  bool write_in_progress_ = false;
 };
 
 // A pipe that can you can use to carry a std::shared_ptr.  The pipe
@@ -75,13 +102,54 @@ public:
   SharedPtrPipe() = default;
   SharedPtrPipe(int r, int w) : Pipe(r, w) {}
 
-  absl::StatusOr<std::shared_ptr<T>> Read() {
+  absl::StatusOr<std::shared_ptr<T>> Read(co::Coroutine *c = nullptr) {
     char buffer[sizeof(std::shared_ptr<T>)];
-    ssize_t n = read(ReadFd().Fd(), buffer, sizeof(buffer));
-    if (n <= 0) {
-      return absl::InternalError(
-          absl::StrFormat("Pipe read failed: %s", strerror(errno)));
+    size_t length = sizeof(buffer);
+    size_t total = 0;
+    if (c != nullptr) {
+      // If another coroutine is already reading, wait for it to finish.
+      while (read_in_progress_) {
+        c->Yield();
+      }
+      read_in_progress_ = true;
     }
+    while (total < length) {
+      if (c != nullptr) {
+        // Coroutines do a context switch before reading.  If we use PollAndWait
+        // we can get starvation.
+        int fd = c->Wait(read_.Fd(), POLLIN);
+        if (fd != read_.Fd()) {
+          read_in_progress_ = false;
+          return absl::InternalError("Interrupted");
+        }
+      }
+      ssize_t n = ::read(read_.Fd(), buffer + total, length - total);
+      if (n == 0) {
+        // EOF
+        read_in_progress_ = false;
+        return absl::InternalError("EOF");
+      }
+      if (n == -1) {
+        if (errno == EINTR) {
+          continue;
+        }
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+          if (c == nullptr) {
+            read_in_progress_ = false;
+            return absl::InternalError("Read would block");
+          }
+          if (read_.IsNonBlocking()) {
+            int fd = c->Wait(read_.Fd(), POLLIN);
+            if (fd != read_.Fd()) {
+              read_in_progress_ = false;
+              return absl::InternalError("Interrupted");
+            }
+          }
+        }
+      }
+      total += n;
+    }
+    read_in_progress_ = false;
     // Ref count = N + 1.
     auto copy = *reinterpret_cast<std::shared_ptr<T> *>(buffer);
     auto *p = reinterpret_cast<std::shared_ptr<T> *>(buffer);
@@ -90,7 +158,7 @@ public:
   }
 
   // This makes the pipe an owner of the pointer.
-  absl::Status Write(std::shared_ptr<T> p) {
+  absl::Status Write(std::shared_ptr<T> p, co::Coroutine *c = nullptr) {
     // On entry, ref count for p = N
     char buffer[sizeof(std::shared_ptr<T>)];
 
@@ -99,12 +167,54 @@ public:
     // in-transit reference.
     new (buffer) std::shared_ptr<T>(p);
 
-    // Ref count = N+1
-    ssize_t n = write(WriteFd().Fd(), buffer, sizeof(buffer));
-    if (n <= 0) {
-      return absl::InternalError(
-          absl::StrFormat("Pipe write failed: %s", strerror(errno)));
+    if (c != nullptr) {
+      // If another coroutine is already writing, wait for it to finish.
+      while (write_in_progress_) {
+        c->Yield();
+      }
+      write_in_progress_ = true;
     }
+    size_t total = 0;
+    size_t length = sizeof(buffer);
+    while (total < length) {
+      if (c != nullptr) {
+        // When writing we use PollAndWait to cause the write to happen as soon
+        // as possible.  This speeds up writes as there is no context switch
+        // before the write.
+        int fd = c->PollAndWait(write_.Fd(), POLLOUT);
+        if (fd != write_.Fd()) {
+          write_in_progress_ = false;
+          return absl::InternalError("Interrupted");
+        }
+      }
+      ssize_t n = ::write(write_.Fd(), buffer + total, length - total);
+      if (n == 0) {
+        // EOF
+        write_in_progress_ = false;
+        return absl::InternalError("EOF");
+      }
+      if (n == -1) {
+        if (errno == EINTR) {
+          continue;
+        }
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+          if (c == nullptr) {
+            write_in_progress_ = false;
+            return absl::InternalError("Write would block");
+          }
+          if (write_.IsNonBlocking()) {
+            int fd = c->Wait(write_.Fd(), POLLOUT);
+            if (fd != write_.Fd()) {
+              write_in_progress_ = false;
+              return absl::InternalError("Interrupted");
+            }
+          }
+        }
+      }
+      total += n;
+    }
+    write_in_progress_ = false;
+    // Ref count = N+1
     return absl::OkStatus();
   }
 };
